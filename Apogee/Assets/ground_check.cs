4e8a185fd5b10a9ce54252c0ff95ca6b0ae65c40
ncasale <casalenm924@gmail.com>
2016-02-03 14:21:00 -0500

    Added Unity Project

---
+++ Apogee/Assets/ground_check.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ground_check : MonoBehaviour {
+
+	//The player
+	GameObject player;
+
+	void Start(){
+		
+		//Find the player
+		player = GameObject.Find("player");
+
+	}
+
+
+	//Check to see if we have collided with the ground
+	void OnTriggerEnter2D(Collider2D other){
+
+		//Check to see if we have hit the ground
+		if (other.tag == "Platform") {
+			player.GetComponent<player_movement> ().inAir = false;
+		}
+	}
+}
